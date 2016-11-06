29a81ad33250604ee00ccb5c39961114af3a7531
MisterZi <ale8270@yandex.ru>
2016-11-06 15:50:34 +0300

    vacancies scaffold_controller init

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,5 @@
 class Skill < ActiveRecord::Base
   has_and_belongs_to_many :vacancies
+
+  validates :name, presence: true
 end
--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -1,3 +1,7 @@
 class Vacancy < ActiveRecord::Base
   has_and_belongs_to_many :skills
+
+  validates :name, :expiration_date, :salary, :contacts, presence: true
+  validates :salary, numericality: true
+  validates :contacts, length: {maximum: 255}
 end
